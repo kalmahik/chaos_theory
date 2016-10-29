9f1f2ace8bfc3da0c8e61c9bdb80be5bde7f82f2
kalmahik <kalmahik@yahoo.com>
2016-10-29 17:06:46 +0300

    Murad

--- app/src/main/java/com/kalmahik/chaos_theory/Level3Activity.java
+++ app/src/main/java/com/kalmahik/chaos_theory/Level3Activity.java
@@ -1,30 +1,44 @@
 package com.kalmahik.chaos_theory;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class Level3Activity extends AppCompatActivity {
     private Button button;
     private int counter = 0;
+    private RelativeLayout mainActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_level_3);
+        setContentView(R.layout.activity_level_2);
 
-        getSupportActionBar().setTitle("Level 3");
+        getSupportActionBar().setTitle("Level 2");
 
         button = (Button) findViewById(R.id.button);
+
+        mainActivity = (RelativeLayout) findViewById(R.id.activity_main);
+        mainActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                error_click();
+            }
+        });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 counter++;
-                if (counter > 19) {
+                click();
+                if (counter > 10) {
                     win();
                     next();
                 }
@@ -32,13 +46,19 @@ public class Level3Activity extends AppCompatActivity {
         });
     }
 
+    public void click() {
+        //Toast.makeText(this, "click", Toast.LENGTH_SHORT).show();
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.RED));
+    }
+
+    public void error_click() {
+        //Toast.makeText(this, "click", Toast.LENGTH_SHORT).show();
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.BLUE));
+
+    }
 
     public void win() {
         Toast.makeText(this, "Congratulations", Toast.LENGTH_SHORT).show();
-    }
-
-    public void click() {
-        Toast.makeText(this, "click", Toast.LENGTH_SHORT).show();
     }
 
     public void next() {
