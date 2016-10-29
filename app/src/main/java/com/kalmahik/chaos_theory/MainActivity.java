cd7f870867221821f6200fa623d8c43cfd3fb1ce
kalmahik <kalmahik@yahoo.com>
2016-10-29 14:15:44 +0300

    2

--- app/src/main/java/com/kalmahik/chaos_theory/MainActivity.java
+++ app/src/main/java/com/kalmahik/chaos_theory/MainActivity.java
@@ -2,12 +2,30 @@ package com.kalmahik.chaos_theory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.level_1);
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                win();
+            }
+        });
+    }
+
+
+    public void win(){
+        Toast.makeText(this, "Congratulations", Toast.LENGTH_SHORT).show();
     }
 }
