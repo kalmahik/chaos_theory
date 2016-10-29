8116c140b115c5bca93cda98be376f54797c9f69
kalmahik <kalmahik@yahoo.com>
2016-10-29 14:20:15 +0300

    2

---
+++ app/src/main/java/com/kalmahik/chaos_theory/Level2Activity.java
@@ -0,0 +1,31 @@
+package com.kalmahik.chaos_theory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Level2Activity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_1);
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                win();
+            }
+        });
+    }
+
+
+    public void win(){
+        Toast.makeText(this, "Congratulations", Toast.LENGTH_SHORT).show();
+    }
+}
